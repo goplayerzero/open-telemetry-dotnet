1340992c2a4d2c0dc063729673a4645a25dfb248
kamleshplayerzero <106535429+kamleshplayerzero@users.noreply.github.com>
2024-07-02 19:36:28 +0530

    Demonstrate activity source usage (#2)
    Activity source added in application to help demonstrate more advanced concepts like customization of the tracestate attributes during authentication.

--- aspdotnet/Program.cs
+++ aspdotnet/Program.cs
@@ -1,5 +1,4 @@
 using System.Globalization;
-
 using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
@@ -7,7 +6,7 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,7 +29,7 @@ builder.Logging.AddOpenTelemetry(options =>
         .SetResourceBuilder(
             ResourceBuilder.CreateDefault()
                 .AddService(serviceName))
-//        .AddConsoleExporter()
+    //    .AddConsoleExporter()
         .AddOtlpExporter(options =>
             {
                 options.Endpoint = new Uri(otelEndpoint + "/v1/logs");
@@ -38,65 +37,45 @@ builder.Logging.AddOpenTelemetry(options =>
                 options.Protocol = OtlpExportProtocol.HttpProtobuf;
             });
 });
+
 builder.Services.AddOpenTelemetry()
-      .ConfigureResource(resource => resource.AddService(serviceName))
-      .WithTracing(tracing => tracing
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
         .AddSource(serviceName)
         .AddAspNetCoreInstrumentation()
         .AddHttpClientInstrumentation()
-//        .AddConsoleExporter()
+    //    .AddConsoleExporter()
         .AddOtlpExporter(options =>
             {
                 options.Endpoint = new Uri(otelEndpoint + "/v1/traces");
                 options.Headers = otelHeaders;
                 options.Protocol = OtlpExportProtocol.HttpProtobuf;
             }))
-      .WithMetrics(metrics => metrics
-          .AddMeter(serviceName)
-          .AddMeter("System.Net.NameResolution")
-          .AddMeter("System.Net.Http")
-          .AddAspNetCoreInstrumentation()
-          .AddHttpClientInstrumentation()
-//          .AddConsoleExporter()
-          .AddOtlpExporter(options =>
+    .WithMetrics(metrics => metrics
+        .AddMeter(serviceName)
+        .AddMeter("System.Net.NameResolution")
+        .AddMeter("System.Net.Http")
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+    //  .AddConsoleExporter()
+        .AddOtlpExporter(options =>
             {
                 options.Endpoint = new Uri(otelEndpoint + "/v1/metrics");
                 options.Headers = otelHeaders;
                 options.Protocol = OtlpExportProtocol.HttpProtobuf;
             }));
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
-
-string HandleRollDice([FromServices]ILogger<Program> logger, string? player)
-{
-    var result = RollDice();
-
-    if (string.IsNullOrEmpty(player))
-    {
-        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
-    }
-    else
-    {
-        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
-    }
-
-    return result.ToString(CultureInfo.InvariantCulture);
-}
-
-int RollDice()
-{
-    int dice = Random.Shared.Next(1, 7);
-    if (dice == 4) {
-        throw new Exception("Error");
-    }
-    return dice;
-}
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseCors(MyAllowSpecificOrigins);
 
-app.MapGet("/rolldice/{player?}", HandleRollDice);
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
---
+++ aspdotnet/model/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_simple.model
+{
+    public class User
+    {        
+        public int UserId {get; set;}
+
+        public string? Email {get; set;}
+
+        public string? Name {get; set;}
+    }
+}
